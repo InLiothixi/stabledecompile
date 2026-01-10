34b82da5c42fc3ff6b37ee5055e4e9f738ddbe37
InLiothixi <inliothixieOfficial@gmail.com>
2026-01-10 23:10:32 +0800

    Forgot to unmute mybad

--- SexyAppFramework/SexyAppBase.cpp
+++ SexyAppFramework/SexyAppBase.cpp
@@ -293,7 +293,7 @@ SexyAppBase::SexyAppBase()
 	mAlphaDisabled = false;	
 	mDebugKeysEnabled = false;
 	mOldWndProc = 0;
-	mNoSoundNeeded = true;
+	mNoSoundNeeded = false;
 	mWantFMod = false;
 
 	mSyncRefreshRate = 100;
